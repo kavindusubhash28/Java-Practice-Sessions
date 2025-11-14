bf1c4863b44ce7f0ac12b2930c64518f929a4693
kavindu subhash <70571627+kavindusubhash28@users.noreply.github.com>
2025-11-14 21:41:01 +0530

    java arithmetic operator exercises

---
+++ Exercises/src/ArithmeticOperator.java
@@ -0,0 +1,42 @@
+public class ArithmeticOperator {
+    public static void main(String[] args) {
+
+     // Arithmetic Operators: +, -,*, /, %
+        //int x = 10;
+        //int y = 2;
+        //int z;
+
+        //z = x + y;
+        //z = x - y;
+        //z = x * y;
+        //z = x / y;
+        //z = x % y;
+
+      // Augmented Assignment Operators: +=, -=, *=, /=, %=
+
+        //int x = 10;
+        //int y = 3;
+
+        //x += y;
+        //x -= y;
+        //x *= y;
+        //x /= y;
+        //x %= y;
+
+      // Increment and Decrement Operators: ++, --
+
+       // int x = 1;
+
+        //x +=1;
+        //x++;
+        //x--;
+        //x--;
+
+      // Oredr of operations: Parentheses, Exponents, Multiplication and Division, Addition and Subtraction
+
+        double result = 3 + 4 * (7-5) / 2.0;
+
+
+        System.out.println(result);
+    }
+}
