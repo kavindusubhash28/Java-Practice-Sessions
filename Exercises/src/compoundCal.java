4cddf535760092133259516ef90664cc19f4444e
kavindu subhash <70571627+kavindusubhash28@users.noreply.github.com>
2025-11-26 01:19:49 +0530

    simple compounded interest calculator added

---
+++ Exercises/src/compoundCal.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class compoundCal {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double principal;
+        double rate;
+        int timesCompounded;
+        double amount;
+
+        System.out.print("Enter the principal amount: ");
+        principal = scanner.nextDouble();
+
+        System.out.print("Enter the interest rate (in %): ");
+        rate = scanner.nextDouble() / 100;
+
+        System.out.print("Enter the # of times compounded per year: ");
+        timesCompounded = scanner.nextInt();
+
+        System.out.print("Enter the # of years: ");
+        int years = scanner.nextInt();
+
+        amount = principal * Math.pow(1 + rate / timesCompounded, timesCompounded * years);
+
+        System.out.printf("The amount after %d years is $%.2f", years, amount);
+
+        scanner.close();
+    }
+}
