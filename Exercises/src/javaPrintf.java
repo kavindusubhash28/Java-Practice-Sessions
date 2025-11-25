0863060364fd57563c6c7abc0f2e3ecea9b0004a
kavindu subhash <70571627+kavindusubhash28@users.noreply.github.com>
2025-11-26 01:08:55 +0530

    printf() method added

---
+++ Exercises/src/javaPrintf.java
@@ -0,0 +1,39 @@
+public class javaPrintf {
+    public static void main(String[] args){
+
+        //printf() = is a method used to format output
+        /*
+        String name = "Spongebob";
+        char firstLetter = 'S';
+        int age = 30;
+        double height = 60.5;
+        boolean isEmployed = true;
+
+        System.out.printf("Hello %s\n" , name);
+        System.out.printf("Your name starts with a %c\n", firstLetter);
+        System.out.printf("You are %d years old\n", age);
+        System.out.printf("You are %f inches tall\n", height);
+        System.out.printf("Employed: %b\n", isEmployed);
+
+        System.out.printf("%s is %d years old", name, age); */
+
+       /* double price1 = 9.99;
+        double price2 = 100.15;
+        double price3 = -54.01;
+
+        System.out.printf("% .2f\n", price1);
+        System.out.printf("% .2f\n", price2);
+        System.out.printf("% .2f\n", price3); */
+
+        int id1 = 1;
+        int id2 = 23;
+        int id3 = 456;
+        int id4 = 7890;
+
+        System.out.printf("%-4d\n", id1);
+        System.out.printf("%-4d\n", id2);
+        System.out.printf("%-4d\n", id3);
+        System.out.printf("%-4d\n", id4);
+
+    }
+}
