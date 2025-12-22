883a72693491f549168782e24445ab9d2f45b5bf
kavindu subhash <70571627+kavindusubhash28@users.noreply.github.com>
2025-12-22 23:37:54 +0530

    java methods learned

---
+++ Exercises/src/methods.java
@@ -0,0 +1,24 @@
+public class methods {
+    public static void main(String[] args) {
+
+        square(3);
+
+
+//        String name = "John";
+//        int age = 25;
+//
+//        happyBirthday(name, age);
+//    }
+//
+//    static void happyBirthday(String name, int age) {
+//        System.out.println("Happy Birthday to you!");
+//        System.out.printf("Happy Birthday dear %s!\n", name);
+//        System.out.printf("You are %d years old!\n", age);
+//        System.out.println("Happy Birthday to you!\n");
+//    }
+    }
+    static void happyBirthday(String name, int age){}
+    static double square(double number) {
+        return number * number;
+    }
+}
