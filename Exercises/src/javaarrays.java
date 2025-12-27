121df302877ab404cb8ab5cd403d247321825831
kavindu subhash <70571627+kavindusubhash28@users.noreply.github.com>
2025-12-27 22:57:11 +0530

    java arrays learned

---
+++ Exercises/src/javaarrays.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class javaarrays {
+    public static void main(String[] args) {
+
+        String[] fruits = {"apple","orange","banana", "coconut"};
+
+        //fruits[0] = "pineapple";
+        //int numOfFruits = fruits.length;
+
+//        Arrays.sort(fruits);
+//        Arrays.fill(fruits, "kiwi");
+
+        for(String fruit : fruits){
+            System.out.println(fruit);
+        }
+
+//        for(int i = 0; i < fruits.length; i++){
+//            System.out.print(fruits[i] + " ");
+//        }
+
+
+//        System.out.println(numOfFruits);
+    }
+}
