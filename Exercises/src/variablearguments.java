a19329e435ddbc453478ca983a61caeac16dddf8
kavindu subhash <70571627+kavindusubhash28@users.noreply.github.com>
2025-12-28 23:09:33 +0530

    variablearguments learned

---
+++ Exercises/src/variablearguments.java
@@ -0,0 +1,26 @@
+public class variablearguments {
+    public static void main(String[] args) {
+
+        System.out.println(average(1,2,3,4));
+    }
+//    static int add(int...numbers){
+//
+//        int sum = 0;
+//        for(int number : numbers){
+//            sum+= number;
+//        }
+//
+//        return sum;
+//
+//    }
+
+    static double average(double...numbers){
+
+        double sum = 0;
+        for(double number : numbers){
+            sum += number;
+        }
+
+        return sum / numbers.length;
+    }
+}
