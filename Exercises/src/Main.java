58a55bf0e481972b169705e49c8bbd05c851b46a
kavindu subhash <70571627+kavindusubhash28@users.noreply.github.com>
2026-01-05 20:23:21 +0530

    oop learned

---
+++ Exercises/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args){
+
+        Car car1 = new Car();
+        Car car2 = new Car();
+
+        System.out.println(car1.make + " " + car1.model);
+        System.out.println(car2.make + " " + car2.model);
+
+//        System.out.println(car.model);
+//        System.out.println(car.make);
+//        System.out.println(car.year);
+//        System.out.println(car.price);
+//        System.out.println(car.isRunning);
+
+//        System.out.println(car.isRunning);
+//        car.start();
+//        System.out.println(car.isRunning);
+//        car.stop();
+//        System.out.println(car.isRunning);
+
+//        car.drive();
+//        car.brake();
+    }
+}
